e0a82630e3156631099d565165ce19b061d25dd4
Patrick Shaikh <patrick.shaikh@mayflower.de>
2023-03-23 16:44:19 +0100

    change author name

---
+++ packages/backend/src/main/resources/changesets/fixtures-1.1.sql
@@ -0,0 +1,53 @@
+INSERT INTO public.room (id, name)
+VALUES
+(0, 'Computer Lab'),
+(1, 'Just a Toilet'),
+(2, 'Public Stage');
+
+INSERT INTO
+    public.session (
+    id,
+    room_id,
+    name,
+    description,
+    abstract,
+    start_timestamp,
+    end_timestamp
+)
+VALUES
+    (
+        0,
+        1,
+        'Workshop - Analyzing Shit for dummies',
+        'Together we will be analyzing a life example.',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        1679562000,
+        1679565600
+    ),
+    (
+        1,
+        1,
+        'Takin'' a dump together',
+        'A short Session for people with human desires',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        1679580000,
+        1679583600
+    ),
+    (
+        2,
+        0,
+        'Workshop - Lets deploy to Kubernetes... for dummies',
+        'Dummies will learn what a Kubernetes Deployment consists of.',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        1679580000,
+        1679583600
+    ),
+    (
+        3,
+        2,
+        'DND - Drinks n'' Drugs',
+        'Party hard until open end.',
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        1679598000,
+        1679619600
+    );
