c7fbb97c53571ed6a6d0191322da7cd1d3965444
MareWalzl <maria.walzl@mayflower.de>
2023-03-27 12:19:26 +0200

    add now line

--- packages/frontend/src/views/TimeTableView.vue
+++ packages/frontend/src/views/TimeTableView.vue
@@ -6,13 +6,21 @@
       <div>Mittwoch</div>
     </div>
     <div class="relative mt-16">
-      <div
-        v-for="time in times"
-        :key="time.toString()"
-        class="border-t border-slate-600 flex justify-end items-center h-12 bg-slate-900 p-3 text-sm text-slate-300"
-        :class="{ 'border-t-2': time.getMinutes() === 0 }"
-      >
-        {{ time.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' }) }}
+      <div>
+        <div
+          v-for="time in times"
+          :key="time.toString()"
+          class="border-t border-slate-600 flex justify-end items-center h-12 bg-slate-900 p-3 text-sm text-slate-300"
+          :class="{ 'border-t-2': time.getMinutes() === 0 }"
+        >
+          {{ time.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' }) }}
+        </div>
+        <div
+          class="absolute h-0.5 bg-slate-100 w-full"
+          :style="`top: ${timelineToNowDiff}rem`"
+        >
+          <div class="h-3.5 aspect-square rounded-full bg-slate-100 -mt-1.5 -ml-2"></div>
+        </div>
       </div>
       <div class="absolute flex w-full pr-14 -top-14">
         <div class="flex-1 p-2" v-for="item in [0, 1, 2]" :key="item">
@@ -25,8 +33,8 @@
         <div class="flex-1 px-2" v-for="item in [0, 1, 2]" :key="item">
           <div class="relative">
             <div
-              class="w-full bg-red-500 p-2 rounded-md h-[9rem] absolute"
-              :style="`height: ${session.duration * 3}rem; top: ${session.start * 3}rem`"
+              class="w-full bg-red-500 p-2 rounded-md absolute"
+              :style="`height: ${session.duration * 3 - 0.2}rem; top: ${session.start * 3}rem`"
               v-for="session in sessions"
               :key="session.name"
             >
@@ -40,12 +48,17 @@
 </template>
 
 <script setup lang="ts">
-import { eachMinuteOfInterval } from 'date-fns'
+import { eachMinuteOfInterval, differenceInMinutes } from 'date-fns'
+import { computed, onUnmounted, ref } from 'vue';
+
+const timelineStart = new Date(2023, 2, 27, 11)
+const timelineEnd = new Date(2023, 2, 27, 16)
+
 function generateTimelineArray() {
   const result = eachMinuteOfInterval(
     {
-      start: new Date(2014, 9, 14, 13),
-      end: new Date(2014, 9, 14, 16, 3)
+      start: timelineStart,
+      end: timelineEnd
     },
     { step: 15 }
   )
@@ -58,7 +71,7 @@ const sessions = [
   {
     name: 'coole session',
     start: 0,
-    duration: 3
+    duration: 4
   },
   {
     name: 'coole session',
@@ -66,4 +79,25 @@ const sessions = [
     duration: 1
   }
 ]
+
+const now = ref(new Date())
+
+const interval = setInterval(()=>{
+    const newDate = new Date()
+    if (newDate.getSeconds() === 0) {
+        now.value = newDate
+    }
+}, 1000)
+
+const timelineToNowDiff = computed(() => {
+    const diff = differenceInMinutes(now.value, timelineStart)
+    const remDistance = 3 / 15 * diff
+    console.log(diff)
+    return remDistance
+})
+
+onUnmounted(()=> {
+    clearInterval(interval)
+})
+
 </script>
