8f5ad9d9e91895a06ea6583edb90209138a5b8e9
Max T <hustenbonbon@posteo.de>
2023-04-03 16:51:12 +0200

    Add direnv config

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.${system}.default ]; };
+      pkgs-unstable = import nixpkgs-unstable { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "otto-dev";
+        buildInputs = with pkgs; [
+            pkgs.minikube
+            pkgs.skaffold
+            pkgs.openjdk
+            pkgs.nodejs-18_x
+        ];
+      };
+      overlays.default = (final: prev: {
+        skaffold = pkgs-unstable.skaffold;
+      });
+  });
+}
