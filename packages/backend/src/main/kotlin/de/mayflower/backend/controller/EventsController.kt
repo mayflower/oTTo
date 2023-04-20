ea345d5c49ffc1485b1a7403495dece76fad4e97
Lukas Bauer <lukas.bauer@mayflower.de>
2023-04-20 14:24:40 +0200

    update open-api specification; create event Repo

--- packages/backend/src/main/kotlin/de/mayflower/backend/controller/EventsController.kt
+++ packages/backend/src/main/kotlin/de/mayflower/backend/controller/EventsController.kt
@@ -1,29 +1,62 @@
 package de.mayflower.backend.controller
 
+import de.mayflower.backend.entity.EventEntity
 import de.mayflower.backend.entity.RoomEntity
+import de.mayflower.backend.repository.EventRepository
 import de.mayflower.backend.repository.RoomRepository
-import de.mayflower.backend.stubs.api.RoomsControllerDelegate
+import de.mayflower.backend.stubs.api.EventsControllerDelegate
+import de.mayflower.backend.stubs.model.Event
 import de.mayflower.backend.stubs.model.Room
-import de.mayflower.backend.stubs.model.Timeslot
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
-import java.time.LocalDate
-import java.util.Date
+import java.net.URI
+import java.sql.Timestamp
+import java.util.*
 
 @Component("roomsControllerDelegate")
-class RoomController(private val roomRepository: RoomRepository) : RoomsControllerDelegate  {
+class EventsController(private val eventRepository: EventRepository) : EventsControllerDelegate  {
 
+    override fun createEvent(event: Event?): ResponseEntity<Event> {
+        if(event === null) {
+            return ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+
+        val eventEntity = EventEntity.fromDTO(event)
+
+        val eventDatabaseEntity = this.eventRepository.save(eventEntity)
+
+        return ResponseEntity(eventDatabaseEntity.toDTO(), HttpStatus.CREATED)
+    }
+
+    override fun getEventById(id: String): ResponseEntity<Event> {
+        val eventDatabaseEntity = this.eventRepository.findById(id)
+
+        if(eventDatabaseEntity.isEmpty) {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+
+        val event = eventDatabaseEntity.get().toDTO()
+
+        return ResponseEntity(event, HttpStatus.OK)
+    }
+
+    override fun deleteEventById(id: String): ResponseEntity<Unit> {
+        val eventDatabaseEntity = this.eventRepository.deleteById(id)
+
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    /*
     override fun getRooms(): ResponseEntity<List<Room>> {
         val roomsEntityList = this.roomRepository.findAll()
 
         // TODO: get correct timeslot list
-        val rooms = roomsEntityList.map {  Room(it.id, it.name, it.timeslots.map {
+        /*val rooms = roomsEntityList.map {  Room(it.id, it.name, it.timeslots.map {
             Timeslot(it.id, it.room.id, it.start, it.end,  )
-        }) }
+        }) }*/
 
-        return ResponseEntity(rooms, HttpStatus.OK)
+        return ResponseEntity(HttpStatus.OK)
     }
 
     override fun createRoom(room: Room?): ResponseEntity<Room> {
@@ -60,4 +93,5 @@ class RoomController(private val roomRepository: RoomRepository) : RoomsControll
 
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
+     */
 }
--- packages/backend/src/main/kotlin/de/mayflower/backend/entity/EventEntity.kt
+++ packages/backend/src/main/kotlin/de/mayflower/backend/entity/EventEntity.kt
@@ -1,8 +1,10 @@
 package de.mayflower.backend.entity
 
+import de.mayflower.backend.stubs.model.Event
 import jakarta.persistence.*
 import jakarta.validation.constraints.*
 import org.hibernate.annotations.GenericGenerator
+import java.net.URI
 import java.util.Date
 
 @Entity
@@ -16,18 +18,12 @@ class EventEntity(
     var description: String,
 
     @NotNull
+    var details: String,
+
+    @NotNull
     var location: String,
 
-    @NotNull
-    var startDate: Date,
-
-    @NotNull
-    var endDate: Date,
-
     var url: String,
-
-    @ManyToOne(cascade = [CascadeType.ALL])
-    var style: Style = Style(),
 
     @OneToMany(
         //mappedBy = "event",
@@ -39,17 +35,20 @@ class EventEntity(
         cascade = [CascadeType.ALL])
     var sponsors: MutableSet<SponsorEntity> = mutableSetOf<SponsorEntity>(),
 
-    @OneToMany(
-        mappedBy = "event",
-        cascade = [CascadeType.ALL])
-    var sessions: MutableSet<SessionEntity> = mutableSetOf(),
-
 ) {
     @Id
     @GenericGenerator(name = "ulid_generator", strategy = "de.mayflower.backend.helper.UlidGenerator")
     @GeneratedValue(generator = "ulid_generator")
     val id: String = String()
 
-    constructor() : this(String(), String(), String(), Date(), Date(), String())
+    constructor() : this(String(), String(), String(), String(), String())
 
+    fun toDTO(): Event {
+        return Event(this.id, this.name, this.description, this.location, this.details, URI(this.url))
+    }
+    companion object {
+        fun fromDTO(event: Event): EventEntity {
+            return EventEntity(event.name, event.description, event.details, event.location, event.url.toString())
+        }
+    }
 }
